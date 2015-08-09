b4b13b6b6a4809522278c54f2ee488f59c3cc4b8
zedar <zakrzewski.robert@gmail.com>
2015-08-09 21:39:36 +0200

    Initial commit

---
+++ src/main/java/rpex/hadoop/Main.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package rpex.hadoop;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import ratpack.guice.Guice;
+import ratpack.handling.Context;
+import ratpack.handling.Handler;
+import ratpack.handling.RequestId;
+import ratpack.handling.ResponseTimer;
+import ratpack.handling.internal.UuidBasedRequestIdGenerator;
+import ratpack.server.BaseDir;
+import ratpack.server.RatpackServer;
+import rpex.hadoop.configs.HadoopConfig;
+
+/**
+ * Starting point for the hadoop analysis server.
+ */
+public class Main {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+  public static void main(String... args) throws Exception {
+    RatpackServer.start(spec -> spec
+      .serverConfig(builder -> builder
+        .baseDir(BaseDir.find("application.properties"))
+        .props(Main.class.getClassLoader().getResource("application.properties")).env().sysProps()
+        .require("/hadoop", HadoopConfig.class)
+      )
+      .registry(Guice.registry(bindingsSpec -> bindingsSpec
+        .bindInstance(ResponseTimer.decorator())
+      ))
+      .handlers(chain -> chain
+          .all(new Handler() {
+            @Override
+            public void handle(Context ctx) throws Exception {
+              MDC.put("clientIP", ctx.getRequest().getRemoteAddress().getHostText());
+              RequestId.Generator generator = ctx.maybeGet(RequestId.Generator.class).orElse(UuidBasedRequestIdGenerator.INSTANCE);
+              RequestId requestId = generator.generate(ctx);
+              ctx.getRequest().add(RequestId.class, requestId);
+              MDC.put("requestId", requestId.getId());
+
+              ctx.next();
+            }
+          })
+          .prefix("v1", chain1 -> chain1
+              .get("api-def", ctx -> {
+                LOGGER.debug("GET API_DEF.JSON");
+                ctx.render(ctx.file("public/apidef/api-def.json"))
+              })
+          )
+      )
+    );
+  }
+}
